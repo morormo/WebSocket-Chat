1d212f36fabb51be6b312237878e649e17d84d38
Morormo <morormo@gmail.com>
2020-05-15 17:31:53 +0200

    ADD Server

--- client/app.js
+++ client/app.js
@@ -11,6 +11,10 @@ loginForm.addEventListener('submit', (event) => {
   login(event);
 });
 
+addMessageForm.addEventListener('submit', (event) => {
+  sendMessage(event);
+});
+
 const login = () => {
   event.preventDefault();
 
@@ -21,4 +25,29 @@ const login = () => {
     loginForm.classList.remove('show');
     messagesSection.classList.add('show');
   }
+};
+
+const sendMessage = () => {
+  event.preventDefault();
+
+  if(messageContentInput.value == ''){
+    alert('Type your message');
+  } else {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput.value= '';
+  }
+};
+
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if(author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+    <div class="message__content">
+      ${content}
+    </div>
+  `;
+  messagesList.appendChild(message);
 }
